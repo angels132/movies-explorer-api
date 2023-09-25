10092eccc6e9dd9e0b7350e2e7f571eb08eeb00d
Ilya <ravilov.ilia@yandex.ru>
2023-09-25 17:34:56 +0700

    Настроил cors,readme, добавил валидацию

--- models/movie.js
+++ models/movie.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-const { ObjectId } = mongoose.Schema.Types;
+const { ObjectId } = Schema.Types;
 
-const { URL_REGEX } = require('../utils/constants');
+const { URL_REGEX } = require('../utils/validation');
 
 const movieSchema = new Schema(
   {
